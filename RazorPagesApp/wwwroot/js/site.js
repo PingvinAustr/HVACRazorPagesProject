052f11acb219180852a445d54bbd2452f4564cf8
Yaroslav <yarik.chushenko1@gmail.com>
2022-07-06 14:37:03 +0300

    Initial commit

---
+++ RazorPagesApp/wwwroot/js/site.js
@@ -0,0 +1,8 @@
+﻿
+function SwitchTab(go_to_tab_number) {
+    //sessionStorage.setItem('go_to_tab_number', go_to_tab_number);
+    //alert(sessionStorage.getItem('go_to_tab_number'));
+    document.cookie = "go_to_tab_number=" + go_to_tab_number;
+    //alert(document.cookie);
+    document.location.reload();
+}
